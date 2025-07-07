e5987640cffb7253fe4423c1f367b3982cde2280
Noah CHANTIN <n.chantin@intalio-fr.com>
2025-07-07 14:11:52 +0200

    Finish, vraiment cette fois

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,11 +4,10 @@ import {
   provideZonelessChangeDetection,
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
-<<<<<<< HEAD
-import { provideHttpClient } from '@angular/common/http';
-=======
-import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
->>>>>>> a2ce3dc3bf568ad0b7f6b7d305d04986ef8dfe70
+import {
+  provideHttpClient,
+  withInterceptorsFromDi,
+} from '@angular/common/http';
 
 import { routes } from './app.routes';
 
@@ -17,12 +16,6 @@ export const appConfig: ApplicationConfig = {
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
     provideRouter(routes),
-<<<<<<< HEAD
-    provideHttpClient(),
+    provideHttpClient(withInterceptorsFromDi()),
   ],
 };
-=======
-    provideHttpClient(withInterceptorsFromDi())
-  ]
-};
->>>>>>> a2ce3dc3bf568ad0b7f6b7d305d04986ef8dfe70
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,19 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { UploadComponent } from './components/upload/upload-component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, UploadComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App {
-<<<<<<< HEAD
   protected title = 'Smart Analyser';
 }
-=======
-  protected title = 'Smart_Analyser';
-}
->>>>>>> a2ce3dc3bf568ad0b7f6b7d305d04986ef8dfe70
